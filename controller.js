8ceb6f1bf65cc3c4d0b97b9c96f28b08d59ef267
oliverlai55 <oliverlai55@gmail.com>
2015-11-12 13:51:42 -0500

    Adding new index.html and sass
    Also added bootstrap, angular js

--- controller.js
+++ controller.js
@@ -1,7 +1,11 @@
 //Angular App Module and controller
 angular.module('myApp',[]).controller('mapCtrl',function($scope){
 
-
+	var mapOption = {
+		zoom: 4,
+		//center of the us
+		center: new google.maps.LatLng(40.0000, -98.0000), mapTypeId: google.maps.mapTypeId.TERRAIN
+	}
 
 
 });
